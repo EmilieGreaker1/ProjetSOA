db45a805b051a727c791c9ed5b5746e5da4d9f16
EmilieGreaker1 <emigre01@hotmail.com>
2024-11-24 16:56:15 +0100

    loginService update

---
+++ backend/LogInService/src/test/java/fr/insa/msa/logInService/LogInServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.msa.logInService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogInServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
