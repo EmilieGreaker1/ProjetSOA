1f5d0dfb01959ee9a75a4569779255ae642bc524
EmilieGreaker1 <emigre01@hotmail.com>
2024-11-26 17:59:31 +0100

    admin backend advancements

---
+++ backend/AdminService/src/main/java/fr/insa/msa/AdminService/models/Request.java
@@ -0,0 +1,85 @@
+package fr.insa.msa.AdminService.models;
+
+public class Request {
+	
+	private int id;
+	private int userId;
+	private int volunteerId;
+	private String text;
+	private String status;
+	private String adminComment;
+	private String time;
+	
+	public Request(int id, int userId, String text) {
+		this.id = id;
+        this.userId = userId;
+        this.text = text;
+        this.status = "pendingAdmin";
+    }
+	
+	public Request(int id, int userId, int volunteerId, String text, String status, String adminComment, String time) {
+        this.id = id;
+        this.userId = userId;
+        this.volunteerId = volunteerId;
+        this.text = text;
+        this.status = status;
+        this.adminComment = adminComment;
+        this.time = time;
+    }
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public int getVolunteerId() {
+		return volunteerId;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public String getAdminComment() {
+		return adminComment;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public void setVolunteerId(int volunteerId) {
+		this.volunteerId = volunteerId;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public void setAdminComment(String admComment) {
+		this.adminComment = admComment;
+	}
+	
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
