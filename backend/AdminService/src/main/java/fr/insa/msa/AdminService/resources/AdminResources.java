c5daa229fb7b4b277ea7f6a8fc4de5691459e9d7
EmilieGreaker1 <emigre01@hotmail.com>
2024-11-26 20:57:35 +0100

    AdminService backend finished

--- backend/AdminService/src/main/java/fr/insa/msa/AdminService/resources/AdminResources.java
+++ backend/AdminService/src/main/java/fr/insa/msa/AdminService/resources/AdminResources.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.insa.msa.AdminService.models.Request;
+import fr.insa.msa.AdminService.models.SpontaneousHelp;
 
 @RestController
 @CrossOrigin(origins = {"http://127.0.0.1:9090", "http://localhost:9090"})
@@ -72,16 +74,24 @@ public class AdminResources {
 	}
 	
 	@PutMapping("/updateReq")
-	//Ikke path heller body... finn hvor vi gjorde det...
-	public void updateRequest(@PathVariable("id") int id) {
-		
+	public void updateRequest(@RequestBody Request req) {
+		try {
+			Connection connection = DriverManager.getConnection(getDbUrl(), getDbUsername(), getDbPassword());
+			Statement stmt = connection.createStatement(); 
+			stmt.executeQuery("UPDATE Requests SET status = '" + req.getStatus() + "', adminComment = '" + req.getAdminComment() + "' WHERE id = " + req.getId());
+			
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	// Get all SpontaneousHelp awaiting admin action from the database
 	@GetMapping("/allWaitingSpontaneousHelp")
-	public List<Request> getSpontaneousHelp() {
+	public List<SpontaneousHelp> getSpontaneousHelp() {
 		
-		List<Request> reqList = new ArrayList<>();
+		List<SpontaneousHelp> spontHelpList = new ArrayList<>();
 		
 		try {
 			Connection connection = DriverManager.getConnection(getDbUrl(), getDbUsername(), getDbPassword());
@@ -89,7 +99,7 @@ public class AdminResources {
 			ResultSet rs = stmt.executeQuery("SELECT * FROM SpontaneousHelp WHERE status = 'pendingAdmin';");
 			
 			while(rs.next()) {
-				reqList.add(new Request(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7)));
+				spontHelpList.add(new SpontaneousHelp(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7)));
 			}
 			
 			connection.close();
@@ -98,6 +108,20 @@ public class AdminResources {
 			e.printStackTrace();
 		}
 
-		return reqList;
+		return spontHelpList;
+	}
+	
+	@PutMapping("/updateSpontHelp")
+	public void updateSpontaneousHelp(@RequestBody SpontaneousHelp spontHelp) {
+		try {
+			Connection connection = DriverManager.getConnection(getDbUrl(), getDbUsername(), getDbPassword());
+			Statement stmt = connection.createStatement(); 
+			stmt.executeQuery("UPDATE SpontaneousHelp SET status = '" + spontHelp.getStatus() + "', adminComment = '" + spontHelp.getAdminComment() + "' WHERE id = " + spontHelp.getId());
+			
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- backend/ConfigServer/src/main/java/fr/insa/ms/server/config/ConfigServer/controller/ConfigController.java
+++ backend/ConfigServer/src/main/java/fr/insa/ms/server/config/ConfigServer/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package fr.insa.ms.server.config.ConfigServer.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
